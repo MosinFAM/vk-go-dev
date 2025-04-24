40ae4a70f77747ee66574ed1c8031ed60fc934b7
MosinFAM <alexfilinxxx@mail.ru>
2025-04-24 18:50:39 +0300

    правки

--- 04_hw/taskbot/bot.go
+++ 04_hw/taskbot/bot.go
@@ -7,29 +7,42 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 
 	tgbotapi "github.com/skinass/telegram-bot-api/v5"
 )
 
-const help = `
-/tasks - посмотреть все задачи
-/new XXX YYY ZZZ -создать новую задачу
-/assign_$ID - сделать пользователя исполлнителем задачи
-/unassign_$ID - удалить задачу у текущего исполнителя
-/resolve_$ID - выполнить задачу, удалить ее из списка
-/my - показать задачи, которые мне поручены
-/owner - показать задачи, которые были созданы мной
-`
+const (
+	msgHelp = `
+		/tasks - посмотреть все задачи
+		/new XXX YYY ZZZ -создать новую задачу
+		/assign_$ID - сделать пользователя исполлнителем задачи
+		/unassign_$ID - удалить задачу у текущего исполнителя
+		/resolve_$ID - выполнить задачу, удалить ее из списка
+		/my - показать задачи, которые мне поручены
+		/owner - показать задачи, которые были созданы мной
+	`
+	msgGreeting       = "Привет! Я твой менеджер задач!"
+	msgNoTasks        = "Нет задач"
+	msgNotAssignee    = "Задача не на вас"
+	msgAccepted       = "Принято"
+	msgNoYourTasks    = "У вас нет задач"
+	msgNoCreatedTasks = "Вы не создавали задачи"
+	msgUnknownCommand = "Я не знаю такую команду"
+	msgLogNoTasks     = "Задачи не существует"
+)
 
 var (
-	BotToken = flag.String("tg.token", "", "token for telegram")
-
-	WebhookURL = flag.String("tg.webhook", "", "webhook addr for telegram")
-	// mu            sync.Mutex
-
+	BotToken   string
+	WebhookURL string
 )
+
+func init() {
+	flag.StringVar(&BotToken, "tg.token", "", "token for telegram")
+	flag.StringVar(&WebhookURL, "tg.webhook", "", "webhook addr for telegram")
+}
 
 type User struct {
 	ID       int64
@@ -42,22 +55,25 @@ type Task struct {
 	Owner    *User
 }
 
-var taskID int64 = 1
-var tasks = make(map[int64]*Task)
-var tasksKeys = make([]int64, 0, 5)
+type TaskManager struct {
+	tasks  map[int64]*Task
+	lastID int64
+}
 
-// Функция для text == /tasks
-func allTasks(tasks map[int64]*Task, tasksKeys []int64, userID int64) string {
+func NewTaskManager() *TaskManager {
+	return &TaskManager{
+		tasks:  make(map[int64]*Task),
+		lastID: 1,
+	}
+}
+
+func (tm *TaskManager) getAllTasks(userID int64) string {
 	var myResponse string
-	rowsCount := 0 // количество строк, для пробелов между ними
-	if len(tasks) > 0 {
-		for _, taskID := range tasksKeys {
-			task, taskExists := tasks[taskID]
+	rowsCount := 0
 
-			if !taskExists {
-				log.Println("Задачи не существует")
-				break
-			}
+	tasks := tm.getSortedTasks()
+	if len(tasks) > 0 {
+		for _, task := range tasks {
 			if rowsCount >= 1 {
 				myResponse += "\n\n"
 			}
@@ -65,24 +81,19 @@ func allTasks(tasks map[int64]*Task, tasksKeys []int64, userID int64) string {
 			rowsCount++
 		}
 	} else {
-		myResponse = "Нет задач"
+		myResponse = msgNoTasks
 	}
 
 	return myResponse
 }
 
-// Функция для text == /owner
-func ownTasks(tasks map[int64]*Task, tasksKeys []int64, userID int64) string {
+func (tm *TaskManager) getOwnTasks(userID int64) string {
 	var myResponse string
 	found := false
-	rowsCount := 0 // количество строк, для пробелов между ними
-	for _, taskID := range tasksKeys {
-		task, taskExists := tasks[taskID]
+	rowsCount := 0
 
-		if !taskExists {
-			log.Println("Задачи не существует")
-			break
-		}
+	tasks := tm.getSortedTasks()
+	for _, task := range tasks {
 		if task.Owner.ID == userID {
 
 			if rowsCount >= 1 {
@@ -94,77 +105,63 @@ func ownTasks(tasks map[int64]*Task, tasksKeys []int64, userID int64) string {
 		}
 	}
 	if !found {
-		myResponse = "Вы не создавали задачи"
+		myResponse = msgNoCreatedTasks
 	}
 
 	return myResponse
 }
 
-// Функция для text == /my
-func myTasks(tasks map[int64]*Task, tasksKeys []int64, userID int64) string {
+func (tm *TaskManager) getMyTasks(userID int64) string {
 	var myResponse string
-	found := false // Есть ли мои таски
-	for _, taskID := range tasksKeys {
-		task, taskExists := tasks[taskID]
+	found := false
 
-		if !taskExists {
-			log.Println("Задачи не существует")
-			break
-		}
+	tasks := tm.getSortedTasks()
+	for _, task := range tasks {
 		if task.Assignee != nil && task.Assignee.ID == userID {
 			myResponse += fmt.Sprintf("%d. %s by @%s\n/unassign_%d /resolve_%d",
-				taskID, task.Title, task.Owner.UserName, taskID, taskID)
+				task.ID, task.Title, task.Owner.UserName, task.ID, task.ID)
 			found = true
 		}
 
 	}
 	if !found {
-		myResponse = "У вас нет задач"
+		myResponse = msgNoYourTasks
 	}
 
 	return myResponse
 }
 
-// Функция для text == /new
-func newTasks(
-	tasks map[int64]*Task, tasksKeys *[]int64, userID int64, userName string, commandText string, taskID *int64,
-) string {
+func (tm *TaskManager) addTasks(userID int64, userName, commandText string) string {
 	var myResponse string
 
 	title := strings.TrimSpace(strings.TrimPrefix(commandText, "/new"))
 
-	// Создаем таску
 	task := Task{
-		ID:    *taskID,
+		ID:    tm.lastID,
 		Title: title,
 		Owner: &User{
 			ID:       userID,
 			UserName: userName,
 		},
 	}
-	tasks[*taskID] = &task
-	*tasksKeys = append(*tasksKeys, *taskID)
+	tm.tasks[tm.lastID] = &task
 
-	myResponse = fmt.Sprintf(`Задача "%s" создана, id=%d`, title, *taskID)
+	myResponse = fmt.Sprintf(`Задача "%s" создана, id=%d`, title, tm.lastID)
 
-	(*taskID)++
+	tm.lastID++
 	return myResponse
 }
 
-// Функция для text == /assign
-func assignTasks(
-	tasks map[int64]*Task, text string, userID int64, userName string,
-) (string, string, int64) {
+func (tm *TaskManager) assignTasks(text string, userID int64, userName string) (string, string, int64) {
 	var myResponse, ownerResponse string
 	var ownerReceiverID int64
 
-	task, _ := taskByID(text, tasks)
+	task, _ := tm.getTaskByID(text)
 
 	if task == nil {
 		return "", "", -1
 	}
 
-	// Если был исполнитель задачи
 	if task.Assignee != nil {
 		ownerReceiverID = task.Assignee.ID
 	} else {
@@ -176,10 +173,8 @@ func assignTasks(
 		UserName: userName,
 	}
 
-	// Сообщение юзеру
 	myResponse = fmt.Sprintf(`Задача "%s" назначена на вас`, task.Title)
 
-	// Второе сообщение
 	if userID != task.Owner.ID {
 		ownerResponse = fmt.Sprintf(`Задача "%s" назначена на @%s`, task.Title, userName)
 	}
@@ -187,12 +182,11 @@ func assignTasks(
 	return myResponse, ownerResponse, ownerReceiverID
 }
 
-// Функция для text == /unassign
-func unassignTasks(tasks map[int64]*Task, text string, userID int64) (string, string, int64) {
+func (tm *TaskManager) unassignTasks(text string, userID int64) (string, string, int64) {
 	var myResponse, ownerResponse string
 	var ownerReceiverID int64
 
-	task, _ := taskByID(text, tasks)
+	task, _ := tm.getTaskByID(text)
 
 	if task == nil {
 		return "", "", 0
@@ -201,27 +195,22 @@ func unassignTasks(tasks map[int64]*Task, text string, userID int64) (string, st
 	ownerReceiverID = task.Owner.ID
 
 	if userID != task.Assignee.ID {
-		myResponse = `Задача не на вас`
+		myResponse = msgNotAssignee
 	} else {
-		// Снимаем исполнителя с задачи
 		task.Assignee = nil
-		myResponse = `Принято`
+		myResponse = msgAccepted
 
-		// Сообщение владельцу задачи
 		ownerResponse = fmt.Sprintf(`Задача "%s" осталась без исполнителя`, task.Title)
 	}
 
 	return myResponse, ownerResponse, ownerReceiverID
 }
 
-// Функция для text == /resolve
-func resolveTasks(
-	tasks map[int64]*Task, text string, userID int64, tasksKeys *[]int64, userName string,
-) (string, string, int64) {
+func (tm *TaskManager) resolveTasks(text string, userID int64, userName string) (string, string, int64) {
 	var myResponse, ownerResponse string
 	var ownerReceiverID int64
 
-	task, id := taskByID(text, tasks)
+	task, id := tm.getTaskByID(text)
 
 	if task == nil {
 		return "", "", 0
@@ -230,14 +219,10 @@ func resolveTasks(
 	ownerReceiverID = task.Owner.ID
 	taskTitle := task.Title
 
-	// Удаляем задачу
-	delete(tasks, id)
-	*tasksKeys = remove(*tasksKeys, id)
+	delete(tm.tasks, id)
 
-	// Сообщение исполнителю задачи
 	myResponse = fmt.Sprintf(`Задача "%s" выполнена`, taskTitle)
 
-	// Сообщение владельцу задачи
 	if userID != ownerReceiverID {
 		ownerResponse = fmt.Sprintf(`Задача "%s" выполнена @%s`, taskTitle, userName)
 	}
@@ -245,29 +230,43 @@ func resolveTasks(
 	return myResponse, ownerResponse, ownerReceiverID
 }
 
-func taskByID(text string, tasks map[int64]*Task) (*Task, int64) {
-	// Парсим команду, получаем ID
+func (tm *TaskManager) getTaskByID(text string) (*Task, int64) {
 	parts := strings.Split(text, "_")
+	if len(parts) < 2 {
+		log.Println("Некорректный формат команды — не найден ID")
+		return nil, 0
+	}
+
 	assignID, err := strconv.Atoi(parts[1])
 	if err != nil {
 		log.Printf("Ошибка конвертации string to int: %v", err)
 	}
 
-	// Получаем задачу по ID
 	id := int64(assignID)
-	task, taskExists := tasks[id]
+	task, taskExists := tm.tasks[id]
 
 	if !taskExists {
-		log.Println("Задачи не существует")
+		log.Println(msgLogNoTasks)
 		return nil, 0
 	}
 
 	return task, id
 }
 
-// Форматирует вывод тасок
+func (tm *TaskManager) getSortedTasks() []*Task {
+	tasks := make([]*Task, 0, len(tm.tasks))
+	for _, task := range tm.tasks {
+		tasks = append(tasks, task)
+	}
+
+	sort.Slice(tasks, func(i, j int) bool {
+		return tasks[i].ID < tasks[j].ID
+	})
+
+	return tasks
+}
+
 func formatTaskResponse(task Task, userID int64) string {
-	// Если у задачи нет исполнителя
 	if task.Assignee == nil {
 		return fmt.Sprintf(
 			"%d. %s by @%s\n/assign_%d",
@@ -277,7 +276,6 @@ func formatTaskResponse(task Task, userID int64) string {
 			task.ID,
 		)
 	}
-	// Если исполнитель задачи - юзер
 	if task.Assignee.ID == userID {
 		return fmt.Sprintf(
 			"%d. %s by @%s\nassignee: я\n/unassign_%d /resolve_%d",
@@ -298,19 +296,6 @@ func formatTaskResponse(task Task, userID int64) string {
 	)
 }
 
-// Удаляет из слайса элемент по значению
-func remove(slice []int64, value int64) []int64 {
-	// Создаем новый срез для хранения элементов без удаляемого значения
-	newSlice := []int64{}
-	for _, v := range slice {
-		if v != value {
-			newSlice = append(newSlice, v) // Добавляем только те элементы, которые не равны value
-		}
-	}
-	return newSlice
-}
-
-// startHTTPServer запускает сервер для обработки вебхуков
 func startHTTPServer() {
 	http.HandleFunc("/state", func(w http.ResponseWriter, r *http.Request) {
 		if _, err := w.Write([]byte("all is working")); err != nil {
@@ -323,13 +308,10 @@ func startHTTPServer() {
 		port = "8081"
 	}
 
-	go func() {
-		log.Fatalln("http err:", http.ListenAndServe(":"+port, nil))
-	}()
+	log.Fatalln("http err:", http.ListenAndServe(":"+port, nil))
 	fmt.Println("start listen :" + port)
 }
 
-// setupWebhook устанавливает вебхук для бота
 func setupWebhook(bot *tgbotapi.BotAPI) error {
 	wh, err := tgbotapi.NewWebhook(WebhookURL)
 	if err != nil {
@@ -345,7 +327,6 @@ func setupWebhook(bot *tgbotapi.BotAPI) error {
 
 func startTaskBot(ctx context.Context) error {
 	// сюда пишите ваш код
-	flag.Parse()
 	bot, err := tgbotapi.NewBotAPI(BotToken)
 	if err != nil {
 		log.Fatalf("NewBotAPI failed: %s", err)
@@ -368,6 +349,8 @@ func startTaskBot(ctx context.Context) error {
 
 	updates := bot.ListenForWebhook("/")
 
+	manager := NewTaskManager()
+
 	// Создаём канал для завершения работы
 	done := make(chan struct{})
 
@@ -378,82 +361,73 @@ func startTaskBot(ctx context.Context) error {
 
 	}()
 
-	for {
-		select {
-		case update := <-updates:
-			if update.Message != nil {
+	for update := range updates {
+		if update.Message != nil {
 
-				log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-				var myResponse, ownerResponse string
-				var msg, ownerMsg tgbotapi.MessageConfig
-				var receiverID, ownerReceiverID int64
+			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+			var myResponse, ownerResponse string
+			var msg, ownerMsg tgbotapi.MessageConfig
+			var receiverID, ownerReceiverID int64
 
-				userID := update.Message.From.ID
-				userName := update.Message.From.UserName
-				text := update.Message.Command()
-				switch {
-				case text == "start":
-					myResponse = `Привет! Я твой менеджер задач!`
-					receiverID = update.Message.Chat.ID
+			userID := update.Message.From.ID
+			userName := update.Message.From.UserName
+			text := update.Message.Command()
+			receiverID = update.Message.Chat.ID
+			switch {
+			case text == "start":
+				myResponse = msgGreeting
 
-				case text == "help":
-					myResponse = fmt.Sprintf("Вот мои команды: %s", help)
-					receiverID = update.Message.Chat.ID
+			case text == "help":
+				myResponse = fmt.Sprintf("Вот мои команды: %s", msgHelp)
 
-				case text == "tasks":
-					myResponse = allTasks(tasks, tasksKeys, userID)
-					receiverID = update.Message.Chat.ID
+			case text == "tasks":
+				myResponse = manager.getAllTasks(userID)
 
-				case text == "owner":
-					myResponse = ownTasks(tasks, tasksKeys, userID)
-					receiverID = update.Message.Chat.ID
+			case text == "owner":
+				myResponse = manager.getOwnTasks(userID)
 
-				case text == "my":
-					myResponse = myTasks(tasks, tasksKeys, userID)
-					receiverID = update.Message.Chat.ID
+			case text == "my":
+				myResponse = manager.getMyTasks(userID)
 
-				case strings.HasPrefix(text, "new"):
-					myResponse = newTasks(tasks, &tasksKeys, userID, userName, update.Message.Text, &taskID)
-					receiverID = update.Message.Chat.ID
+			case strings.HasPrefix(text, "new"):
+				myResponse = manager.addTasks(userID, userName, update.Message.Text)
 
-				case strings.HasPrefix(text, "assign"):
-					receiverID = update.Message.Chat.ID
-					myResponse, ownerResponse, ownerReceiverID = assignTasks(tasks, text, userID, userName)
+			case strings.HasPrefix(text, "assign"):
+				myResponse, ownerResponse, ownerReceiverID = manager.assignTasks(text, userID, userName)
 
-				case strings.HasPrefix(text, "unassign"):
-					receiverID = update.Message.Chat.ID
-					myResponse, ownerResponse, ownerReceiverID = unassignTasks(tasks, text, userID)
+			case strings.HasPrefix(text, "unassign"):
+				myResponse, ownerResponse, ownerReceiverID = manager.unassignTasks(text, userID)
 
-				case strings.HasPrefix(text, "resolve"):
-					receiverID = update.Message.Chat.ID
-					myResponse, ownerResponse, ownerReceiverID = resolveTasks(tasks, text, userID, &tasksKeys, userName)
+			case strings.HasPrefix(text, "resolve"):
+				myResponse, ownerResponse, ownerReceiverID = manager.resolveTasks(text, userID, userName)
 
-				default:
-					myResponse = `Я не знаю такую команду`
-					receiverID = update.Message.Chat.ID
+			default:
+				myResponse = msgUnknownCommand
 
-				}
+			}
 
-				msg = tgbotapi.NewMessage(receiverID, myResponse)
-				if _, err := bot.Send(msg); err != nil {
-					log.Printf("Ошибка отправки сообщения: %v", err)
-				}
-				if ownerResponse != "" {
-					ownerMsg = tgbotapi.NewMessage(ownerReceiverID, ownerResponse)
-					if _, err := bot.Send(ownerMsg); err != nil {
-						log.Printf("Ошибка отправки сообщения владельцу: %v", err)
-					}
+			msg = tgbotapi.NewMessage(receiverID, myResponse)
+			if _, err := bot.Send(msg); err != nil {
+				log.Printf("Ошибка отправки сообщения: %v", err)
+			}
+			if ownerResponse != "" {
+				ownerMsg = tgbotapi.NewMessage(ownerReceiverID, ownerResponse)
+				if _, err := bot.Send(ownerMsg); err != nil {
+					log.Printf("Ошибка отправки сообщения владельцу: %v", err)
 				}
 			}
-		case <-done:
-			fmt.Println("Бот завершает работу")
-			return nil
 		}
 	}
+
+	fmt.Println("Бот завершает работу")
+	return nil
 }
 
 func main() {
-	err := startTaskBot(context.Background())
+	flag.Parse()
+	ctx := context.Background()
+
+	err := startTaskBot(ctx)
 	if err != nil {
 		panic(err)
 	}
